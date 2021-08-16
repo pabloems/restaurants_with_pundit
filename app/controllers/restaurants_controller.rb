0b7749238a593b79967038e8e6b8cce443ecc500
pablo martinez <pablomartinezsepulveda@hotmail.com>
2021-08-16 14:48:14 -0400

    pundit

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,64 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[ show edit update destroy ]
+
+  # GET /restaurants or /restaurants.json
+  def index
+    @restaurants = policy_scope(Restaurant).order(created_at: :desc)
+  end
+
+  # GET /restaurants/1 or /restaurants/1.json
+  def show
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+    authorize @restaurant
+  end
+
+  # GET /restaurants/1/edit
+  def edit
+  end
+
+  # POST /restaurants or /restaurants.json
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+
+    authorize @restaurant
+
+      if @restaurant.save
+        redirect_to @restaurant #Short cut de restaurant_path(@restaurant)
+      else
+        render :new
+      end
+    end
+  end
+
+  # PATCH/PUT /restaurants/1 or /restaurants/1.json
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /restaurants/1 or /restaurants/1.json
+  def destroy
+    @restaurant.destroy
+      redirect_to restaurants_path
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+      authorize @restaurant
+    end
+
+    # Only allow a list of trusted parameters through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name)
+    end
+
---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,37 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all # Ayuda a modificar lo que se ve en vistas ej: scope.where(user: user)
+    end
+  end
+  
+  def show?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    user_is_owner_or_admin?
+  end
+    # El record sobre el cual authorize ( en este caso @restaurant)
+    # Current_user => user
+      # If user == record.user
+      # return true
+      # else
+        # return false
+      # end
+
+   def destroy?
+    user_is_owner_or_admin?
+   end
+
+  private
+
+  def user_is_owner_or_admin?
+    user == record.user || user.admin
+  end
+
+end
